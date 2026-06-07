6dc78e9d6ad9ac2e2da474a9e68e59e31f692436
ghjo <ghjo@teslasystem.co.kr>
2026-06-08 08:27:20 +0900

    Rename Writter.h to define.h

---
+++ define.h
@@ -0,0 +1,56 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#include <QDateTime>
+#include <QObject>
+#include <QDebug>
+enum class LogLevel{
+    INFO, WARN, ERROR
+};
+
+Q_DECLARE_METATYPE(LogLevel)
+
+
+struct ClipInfo
+{
+    QString camName;
+    QString sensorName;
+    QString videoPath;
+    QStringList weather;
+    QStringList event;
+};
+
+
+
+class Writter
+{
+public:
+  static void write(const QString& content, LogLevel level = LogLevel::INFO)
+  {
+      QString timestamp = QDateTime::currentDateTime().toString("yyyyMMdd_hhmmss.zzz");
+
+      if(level == LogLevel::INFO)
+          qDebug().noquote() << timestamp << "[INFO] " << content;
+      else if(level == LogLevel::WARN)
+          qWarning().noquote() << "\033[33m" << timestamp << "[WARN] " << content;
+      else if(level == LogLevel::ERROR)
+          qCritical().noquote() << "\033[31m" << timestamp << "[ERROR] " << content;
+  }
+
+  static void info(const QString& content)
+  {
+      write(content, LogLevel::INFO);
+  }
+
+  static void warn(const QString& content)
+  {
+      write(content, LogLevel::WARN);
+  }
+
+  static void error(const QString& content)
+  {
+      write(content, LogLevel::ERROR);
+  }
+};
+
+#endif // DEFINE_H
